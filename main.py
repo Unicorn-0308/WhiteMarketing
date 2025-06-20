7d8df2852b93c152b58d22eff9366994e480e3f2
Unicorn-0308 <unicorn0308.legend@outlook.com>
2025-06-19 22:51:05 -0500

    Feature: change file uploading component

--- main.py
+++ main.py
@@ -58,49 +58,49 @@ My name is Nathan. How can I assist you today?"""
             st.markdown(message.get("content", ""), unsafe_allow_html=True)
 
     # Get user input`
-    files = st.file_uploader("Upload your KPI data", "csv", True, label_visibility="hidden")
-    # if prompt := st.chat_input("What are you curious about?", accept_file="multiple", file_type="csv"):
-    #     if prompt.text:
-    if prompt := st.chat_input("What are you curious about?"):
-        with st.chat_message("user", avatar="🤵"):
-            st.markdown(prompt, unsafe_allow_html=True)
-        st.session_state.messages.append({"role": "user", "content": prompt})
+    # files = st.file_uploader("Upload your KPI data", "csv", True, label_visibility="hidden")
+    if prompt := st.chat_input("What are you curious about?", accept_file="multiple", file_type="csv"):
+        if prompt.text:
+        # if prompt := st.chat_input("What are you curious about?"):
+            with st.chat_message("user", avatar="🤵"):
+                st.markdown(prompt, unsafe_allow_html=True)
+            st.session_state.messages.append({"role": "user", "content": prompt})
 
-        with st.chat_message("assistant", avatar="🤖"):
-            with st.spinner("The assistant is thinking..."):
-                files_to_send = {}
-                for f in files or []:
-                    files_to_send[f.name] = (f.name, f.getvalue(), "text/csv")
+            with st.chat_message("assistant", avatar="🤖"):
+                with st.spinner("The assistant is thinking..."):
+                    files_to_send = {}
+                    for f in prompt.files or []:
+                        files_to_send[f.name] = (f.name, f.getvalue(), "text/csv")
 
-                response = request(
-                    "post",
-                    "https://whitemarketing.app.n8n.cloud/webhook/3af16ffd-ac9f-4a13-93d9-4bdffe3d949c",
-                    data={
-                        "action": "sendMessage",
-                        "chatInput": prompt,
-                        "sessionId": st.session_state.sessionId,
-                    },
-                    files=files_to_send,
-                    timeout=300,
-                )
+                    response = request(
+                        "post",
+                        "https://whitemarketing.app.n8n.cloud/webhook/3af16ffd-ac9f-4a13-93d9-4bdffe3d949c",
+                        data={
+                            "action": "sendMessage",
+                            "chatInput": prompt.text,
+                            "sessionId": st.session_state.sessionId,
+                        },
+                        files=files_to_send,
+                        timeout=300,
+                    )
 
-                execution = response.json()
+                    execution = response.json()
 
-                result = {}
-                out = 0
-                while True:
-                    result = mongo_.find_one({"id": execution["id"]})
-                    if result or out > 60:
-                        break
-                    time.sleep(5)
-                    out += 1
+                    result = {}
+                    out = 0
+                    while True:
+                        result = mongo_.find_one({"id": execution["id"]})
+                        if result or out > 60:
+                            break
+                        time.sleep(5)
+                        out += 1
 
-                try:
-                    st.markdown(result.get("output", ""), unsafe_allow_html=True)
-                    st.session_state.messages.append({"role": "assistant", "content": result.get("output", "")})
-                    mongo_.delete_one({"id": execution["id"]})
-                    st.rerun()
-                except Exception as e:
-                    st.markdown(str(e), unsafe_allow_html=True)
+                    try:
+                        st.markdown(result.get("output", ""), unsafe_allow_html=True)
+                        st.session_state.messages.append({"role": "assistant", "content": result.get("output", "")})
+                        mongo_.delete_one({"id": execution["id"]})
+                        st.rerun()
+                    except Exception as e:
+                        st.markdown(str(e), unsafe_allow_html=True)
 
 
